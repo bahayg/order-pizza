d330396127be66a2c1bcb0d5cd033896b6f0ad7d
Student <example@example.com>
2019-08-30 14:19:05 -0700

    Add new prototypes to modify the final cost.

--- js/scripts.js
+++ js/scripts.js
@@ -5,65 +5,40 @@ function Pizza(size, toppings, cost) {
   this.cost = cost
 }
 
-Pizza.prototype.sizePrice = function() {
-  if (this.size === "small") {
-    return 10.99;
+Pizza.prototype.sizeValue = function() {
+  if (this.size === "large") {
+    return 4;
   } else if (this.size === "medium") {
-      return 12.99;
+      return 2;
     } else {
-        return 14.99;
+        return 0;
     }
 }
 
-Pizza.prototype.toppingsPrice = function() {
-  var inputtedToppings = $("input:checkbox[name=toppings]:checked").val();
-    alert($("inputtedToppings").length);
+Pizza.prototype.toppingsValue = function() {
+  var inputtedToppings = $("input:checkbox[name=toppings]:checked").length;
+    return (inputtedToppings*2);
   }
 
-Pizza.prototype.calcPrice = function () {
-  return this.sizePrice() + this.toppingsPrice();
+Pizza.prototype.calcPrice = function() {
+  return this.cost + this.sizeValue() + this.toppingsValue();
 }
 
 // User Interface Logic
 $(document).ready(function() {
-  $("#cost").submit(function(event) {
+  $(".form-group").submit(function(event) {
     event.preventDefault();
 
-    var inputtedSize = $("input:checkbox[name=size]:checked").val();
+    var inputtedSize = $("#size").val();
     var inputtedToppings = $("input:checkbox[name=toppings]:checked").val();
 
-    var newPizza = new Pizza (inputtedSize, inputtedToppings, calcPrice());
-    //console.log(newTicket.calcPrice());
+    var newPizza = new Pizza (inputtedSize, inputtedToppings, 10);
 
-    $("#cost").show();
-    $("#cost").text("Your pizza costs " + newPizza.calcPrice());
+    $("#price").show();
+    $("#price").text("Your pizza costs " + newPizza.calcPrice() + "$");
 
     $("input:checkbox").prop("checked", false);
+    $('select').prop('selectedIndex', 0);
 
   });
 });
-
-//   count = 0;
-//    for (var i=0; i<=inputtedToppings.length; i++) {
-//     return [i];
-//    }
-//
-//   return ("input#toppings".length)*2;
-// }
-
-//   $("input:checkbox[name=toppings]:checked").each(function(){
-//         var inputtedToppings = $("input:checkbox[name=toppings]:checked").val();
-//         $("#work-responses").append(workTransitInputs + "<br>");
-//         });
-
-
-// var isCheckedWithGlobalVariable = false;
-// var elements = document.querySelectorAll('input[type=checkbox]');
-// for (var i = 0; i < elements.lenght; i++) {
-//     elements[i].onclick = function() {
-//         if (this.checked == true) {
-//             // checked
-//             isCheckedWithGlobalVariable = true;
-//         }
-//     };
-// }
